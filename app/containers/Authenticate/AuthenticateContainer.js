c962519b1582f4697bac31fe284fe40c2ecd18de
Alex DeCapri <alexdecapri@gmail.com>
2016-11-03 10:12:26 -0600

    connect to redux store

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react'
 import { Authenticate } from 'components'
 import auth from 'helpers/auth'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 // brings in all exported action creators (not the export default reducer in index.js)
 import * as userActionCreators from 'redux/modules/users'
@@ -10,14 +11,18 @@ const AuthenticateContainer = React.createClass({
   propTypes: {
     isFetching: PropTypes.bool.isRequired,
     error: PropTypes.string.isRequired,
+    authUser: PropTypes.func.isRequired,
+    fetchingUser: PropTypes.func.isRequired,
+    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchingUserSuccess: PropTypes.func.isRequired,
   },
   handleAuth () {
-    this.props.dispatch(userActionCreators.fetchingUser())
+    this.props.fetchingUser()
     auth().then((user) => {
-      this.props.dispatch(userActionCreators.fetchingUserSuccess(user.uid, user, Date.now()))
-      this.props.dispatch(userActionCreators.authUser(user.uid))
+      this.props.fetchingUserSuccess(user.uid, user, Date.now())
+      this.props.authUser(user.uid)
     })
-    .catch(() => this.props.dispatch(userActionCreators.fetchingUserFailure(error)))
+    .catch(() => this.props.fetchingUserFailure(error))
   },
   render () {
     console.log('is fetching', this.props.isFetching)
@@ -31,7 +36,7 @@ const AuthenticateContainer = React.createClass({
 })
 
 function mapStateToProps (state) {
-  // this is SICK!!!
+  // this is SICK!!! final state shows exactly what users schema looks like in redux-schema.js
   console.log('state', state)
   return {
     isFetching: state.isFetching,
@@ -39,4 +44,8 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps)(AuthenticateContainer)
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(userActionCreators, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthenticateContainer)
